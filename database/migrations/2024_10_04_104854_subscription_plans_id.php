85be0b7d9c20d4d2a605daed3769a8310ba55ce0
SukhpreetO1 <ssingh77022@gmail.com>
2024-10-04 16:36:34 +0530

    working on adding the home page for users

---
+++ database/migrations/2024_10_04_104854_subscription_plans_id.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription_plans', function (Blueprint $table) {
+            $table->increments('id')->primary();
+            $table->string('plan_id');
+            $table->integer('plan')->nullable()->comment("0 = Monthly, 1 = Yearly");
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription_plans');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render("Users/Dashboard/Dashboard");
+    return Inertia::render("Users/Homepage/Homepage");
     // return Inertia::render('Welcome', [
     //     'canLogin' => Route::has('login'),
     //     'canRegister' => Route::has('register'),
